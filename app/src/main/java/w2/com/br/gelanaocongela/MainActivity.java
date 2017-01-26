c522d6881e7fc67810813d402bb95a7bfddc9c35
yeahsistemas <alexandrels800@gmail.com>
2017-01-26 13:22:30 -0200

    Initial commit

---
+++ app/src/main/java/w2/com/br/gelanaocongela/MainActivity.java
@@ -0,0 +1,63 @@
+package w2.com.br.gelanaocongela;
+
+import android.app.AlarmManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AlarmManager alarmManager;
+    private TimePicker timePicker;
+    private TextView updateAlarm;
+    private Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        this.context = this;
+
+        //iniciar o gerenciador do alarm
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        //iniciar timepicker
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+
+        updateAlarm = (TextView) findViewById(R.id.update_alarm);
+        // teste comit
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
